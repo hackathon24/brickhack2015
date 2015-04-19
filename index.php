1212c19b70a90661a64f654c99af5f9e3bd040d9
Jairo Veloz <jpavelw@gmail.com>
2015-04-19 01:49:32 -0400

    Index.php modified

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     </head>
     <body>
         <?php
-            
-            $data = file_get_contents('https://api.spotify.com/v1/search?q=rihanna&type=artist');
-            echo $data;
+            $amountOfElements = 10;
+            //$response = file_get_contents('https://api.spotify.com/v1/search?q=ri hanna&type=artist');
+            //$response = file_get_contents('https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa');
+            $response = file_get_contents('https://api.spotify.com/v1/search?q=techno&type=playlist&limit='.$amountOfElements.'&market=us');
+            //cho $data;
+            //print_r($data);
+            $json = json_decode($response, true);
+            //print_r($json);
+            for($i = 0; $i < $amountOfElements; ++$i){
+                echo "<p>".$json["playlists"]["items"][$i]["external_urls"]["spotify"]."</p>";
+                echo "<p>".$json["playlists"]["items"][$i]["id"]."</p>";
+                //print_r($json["playlists"]["items"][0]["external_urls"]["spotify"]);
+                //echo "<p>".$json["playlists"]["items"][$i]["images"][0]["url"]."</p>";
+                //echo "<img src=".$json["playlists"]["items"][$i]["images"][0]["url"].">";
+            }
+            echo count($json["playlists"]["items"]);
         ?>
         <p>Hello World!</p>
     </body>
